fa93b2a9c4977205b396982c820c5e1b8c3338a2
Kazi Hasan Al Banna <kazi.hasanalbanna59@gmail.com>
2024-04-20 22:42:28 +0600

    first commit

---
+++ src/car/rental/system/Customer.java
@@ -0,0 +1,24 @@
+
+package car.rental.system;
+
+/**
+ *
+ * @author Banna
+ */
+public class Customer {
+    private String customerId;
+    private String name;
+    
+    public Customer(String customerId, String name){
+        this.customerId = customerId;
+        this.name = name;
+    }
+    
+    public String getCustomerId(){
+        return customerId;
+    }
+    
+    public String getName(){
+        return name;
+    }
+}
---
+++ src/car/rental/system/Main.java
@@ -0,0 +1,24 @@
+
+package car.rental.system;
+
+/**
+ *
+ * @author Banna
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        CarRentalSystem rentalSystem = new CarRentalSystem();
+        
+        Car car1= new Car("C001", "Toyota", "Canry", 60.0);
+        Car car2= new Car("C002", "Honda", "Accord", 70.0);
+        Car car3= new Car("C003", "Mahindra", "Thar", 150.0);
+        
+        rentalSystem.addCar(car1);
+        rentalSystem.addCar(car2);
+        rentalSystem.addCar(car3);
+        
+        rentalSystem.menu();
+    }
+    
+}
